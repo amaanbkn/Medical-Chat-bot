c344ec9dd8ac619770e8a4ef7ff2065e9ae5688b
Amaan Siddiqui <amaansiddiqui014@gmail.com>
2025-11-09 20:31:46 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical_Chat-Bot",
+    version="0.1.0",
+    author="amaanbkn",
+    author_email="amaansiddiqui014@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
